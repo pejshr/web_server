8ac2734e9a8a8fa46b21c0eaf14eddcec304b328
Mne-pr <thousand2000@naver.com>
2024-07-02 22:24:47 +0900

    first

---
+++ src/main/java/com/pejshr/server/MainController.java
@@ -0,0 +1,14 @@
+package com.pejshr.server;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+	@GetMapping("/")
+	@ResponseBody
+	public String index() {
+		return "안녕하세요 sbb에 오신 것을 환영합니다";
+	}
+}
